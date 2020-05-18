af2b5098de2c9fa77258b6a0229ecb0cf500455b
Keturah Howard <keturah.dev@gmail.com>
2020-05-18 13:00:53 -0700

    create post request using express, confirm functionality in postman

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -3,6 +3,13 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 
-app.listen(port, () => {
-  console.log('Server is up and running' + port)
+app.use(express.json()) //auto parses inciming json requests so we can use it in our handlers
+
+app.post('/users', (req, res) => {
+  console.log(req.body)
+  res.send('testing!')
+})
+
+app.listen(port, () => { 
+  console.log('Server is up and running ' + port)
 })

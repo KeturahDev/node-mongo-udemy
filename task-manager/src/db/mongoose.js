2bae0885d0653919d3e8ba25bf484f9a28de8339
Keturah Howard <keturah.dev@gmail.com>
2020-05-18 11:00:33 -0700

    install validator library, explore mongoose velidations

--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const validator = require('validator')
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
   useNewUrlParser: true,
   useCreateIndex: true
@@ -6,11 +7,47 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
 
 const User = mongoose.model('User', {
   name: {
-    type: String
+    type: String,
+    required: true,
+    trim: true
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    lowercase: true,
+    validate(value) {
+      if (!validator.isEmail(value)) {
+        throw new Error("Email is not valid")
+      }
+    }
+  },
+  password: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 7,
+    validate(value) {
+      if(value.toLowerCase().includes("password")) {
+        throw new Error("You cannot have a password containing 'password'.")
+      }
+    }
   },
   age: {
-    type: Number
+    type: Number,
+    default: 0,
+    validate(value) {
+      if(value < 0) {
+        throw new Error("Age must be a positive number")
+      }
+    }
   }
+})
+
+const me = new User({
+  name: "    Markus   ",
+  email: "HEYYOMARK@gmail.com",
+  password: " IlikeTurtles" 
 })
 
 // const me = new User({
@@ -18,11 +55,11 @@ const User = mongoose.model('User', {
 //   age: 21
 // })
 
-// me.save().then((me) => {
-//   console.log(me)
-// }).catch((error) => {
-//   console.log(error)
-// })
+me.save().then((me) => {
+  console.log(me)
+}).catch((error) => {
+  console.log("Error! ",error)
+})
 
 const Task = mongoose.model('Task', {
   description: {
@@ -33,13 +70,13 @@ const Task = mongoose.model('Task', {
   }
 })
 
-const chore = new Task({
-  description: "wash dishes",
-  completed: false
-})
+// const chore = new Task({
+//   description: "wash dishes",
+//   completed: false
+// })
 
-chore.save().then((task) => {
-  console.log(task)
-}).catch((error) => {
-  console.log("Error! ", error)
-})
+// chore.save().then((task) => {
+//   console.log(task)
+// }).catch((error) => {
+//   console.log("Error! ", error)
+// })
